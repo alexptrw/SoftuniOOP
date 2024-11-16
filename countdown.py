3795a628ceff00d07ed4065fd7c2b8fd872ef87b
Aleks <sashaaaptrw@gmail.com>
2024-11-16 21:38:41 +0200

    ex files

---
+++ countdown.py
@@ -0,0 +1,18 @@
+class countdown_iterator:
+    def __init__(self, count):
+        self.count = count
+        self.start = count + 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.start > 0:
+            self.start -= 1
+            return self.start
+        raise StopIteration
+
+
+iterator = countdown_iterator(10)
+for item in iterator:
+    print(item, end=" ")
